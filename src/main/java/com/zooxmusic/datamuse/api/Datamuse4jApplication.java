7005982ff1d42c0074e10a616f59097cce45f7b9
Brian Szucs <brian@zooxmusic.com>
2022-10-15 17:09:05 -0400

    initial commit

---
+++ src/main/java/com/zooxmusic/datamuse/api/Datamuse4jApplication.java
@@ -0,0 +1,13 @@
+package com.zooxmusic.datamuse.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Datamuse4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Datamuse4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zooxmusic/datamuse/api/Datamuse4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zooxmusic.datamuse.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Datamuse4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
